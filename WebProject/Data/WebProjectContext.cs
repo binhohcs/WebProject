f05a6ce1e862a4127c8df0f0e006be72ad4ff08f
binhohcs <binhohcs@hotmail.com>
2020-01-30 11:02:29 -0300

    crud Scaffolding

---
+++ WebProject/Data/WebProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebProject.Models;
+
+namespace WebProject.Data
+{
+    public class WebProjectContext : DbContext
+    {
+        public WebProjectContext (DbContextOptions<WebProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebProject.Models.Department> Department { get; set; }
+    }
+}
--- WebProject/Startup.cs
+++ WebProject/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using WebProject.Data;
 
 namespace WebProject
 {
@@ -33,6 +35,9 @@ namespace WebProject
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebProjectContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebProjectContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
